d11e8b970d0100cf316eb9e474de382b98825153
Adam <armware@gmail.com>
2022-03-29 20:53:42 -0500

    more buttons

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from gi.repository import Gtk
 
 
 class ButtonWindow(Gtk.Window):
-    wifi_prepped = False
-    attack_started = False
+    prepped = False
+    attacking = False
 
     def __init__(self):
         super().__init__(title="Wifite2 UI")
@@ -17,12 +17,12 @@ class ButtonWindow(Gtk.Window):
         hbox = Gtk.Box( spacing=6 )
         self.add(hbox)
 
-        button = Gtk.Button.new_with_label( "Prepare Wifi Card" )
+        button = Gtk.Button.new_with_label( "Wifi Not Prepped" )
         button.connect( "clicked", self.btnPrepare )
         hbox.pack_start(button, True, True, 0)
 
         button = Gtk.Button.new_with_mnemonic( "Start Attack" )
-        button.connect( "clicked", self.btnStart )
+        button.connect( "clicked", self.btnAttack )
         hbox.pack_start(button, True, True, 0)
 
         button = Gtk.Button.new_with_mnemonic( "_Close" )
@@ -30,22 +30,21 @@ class ButtonWindow(Gtk.Window):
         hbox.pack_start(button, True, True, 0)
 
     def btnPrepare(self, button):
-        if self.wifi_prepped:
-            button.set_label( "Wifi ReNormalized" )
+        if self.prepped:
+            button.set_label( "Wifi Not Prepped" )
         else:
-            button.set_label("Wifi Prepped")
-        self.wifi_prepped = not self.wifi_prepped
+            button.set_label( "Wifi Prepped" )
+        self.prepped = not self.prepped
 
-    def btnStart(self, button):
-        if self.attack_started:
-            button.set_label( "Attacking..." )
+    def btnAttack(self, button):
+        if self.attacking:
+            button.set_label( "Stop Attack" )
         else:
-            button.set_label("Stop Attack")
-        self.wifi_prepped = not self.wifi_prepped
+            button.set_label( "Start Attack" )
+        self.attacking = not self.attacking
 
     def btnClose(self, button):
         Gtk.main_quit()
-
 
 win = ButtonWindow()
 win.connect("destroy", Gtk.main_quit)
